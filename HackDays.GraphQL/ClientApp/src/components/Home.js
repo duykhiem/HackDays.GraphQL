434f4d9b8f92244c6387a9bbd271c66a4873fff6
Khiem Nguyen <Khiem.Nguyen@episerver.com>
2021-05-24 11:37:29 +0700

    WIP

--- HackDays.GraphQL/ClientApp/src/components/Home.js
+++ HackDays.GraphQL/ClientApp/src/components/Home.js
@@ -1,44 +1,75 @@
-import React, { Component } from 'react';
-import axios from 'axios'
-import { CarouselImages } from './CarouselImages';
-import { ProductList } from './ProductList'
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import axios from "axios";
+import { CarouselImages } from "./CarouselImages";
+import { ProductList } from "./ProductList";
+import { Link } from "react-router-dom";
+import { useSubscription, gql } from "@apollo/client";
+
+//import { gql } from "graphql-tag";
+
+const COMMENTS_SUBSCRIPTION = gql`
+  subscription {
+    productMutated {
+      id
+      name
+      eventType
+    }
+  }
+`;
+
+function LatestComment() {
+  const { data: { productMutated }, loading } = useSubscription(
+    COMMENTS_SUBSCRIPTION,
+    { variables: { } }
+  );
+  return <h4>New comment: {!loading && productMutated.content}</h4>;
+}
+
 export class Home extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            products: [],
-            loading: true
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: [],
+      loading: true,
+    };
+  }
 
-    componentDidMount() {
-        this.getProducts();
-    }
+  componentDidMount() {
+    this.getProducts();
+  }
 
-    render() {
+  render() {
+    return (
+      <div>
+        <LatestComment />
+        <CarouselImages />
+        {this.state.loading ? (
+          <p>
+            <em>Loading...</em>
+          </p>
+        ) : (
+          <div className="flashSale">
+            <h2>
+              FLASH SALE!!!
+              <small className="float-right">
+                <Link to="/product/add" className="btn btn-default">
+                  Create product
+                </Link>
+              </small>
+            </h2>
+            <ProductList
+              products={this.state.products}
+              onDeleteProduct={(id) => this.deleteProduct(id)}
+            />
+          </div>
+        )}
+      </div>
+    );
+  }
 
-        return (
-            <div>
-                <CarouselImages />
-                {
-                    this.state.loading
-                        ? <p><em>Loading...</em></p> :
-                        <div className="flashSale">
-                            <h2>
-                                FLASH SALE!!!
-                                <small className="float-right"><Link to="/product/add" className="btn btn-default" >Create product</Link></small>
-                            </h2>
-                            <ProductList products={this.state.products} onDeleteProduct={(id) => this.deleteProduct(id)} />
-                        </div>
-                }
-            </div>
-        );
-    }
-
-    async getProducts() {
-        const body = {
-            query: `query {
+  async getProducts() {
+    const body = {
+      query: `query {
               products {
                 id,
                 name,
@@ -47,31 +78,29 @@ export class Home extends Component {
                 price
               }
             }`,
-            variables: {}
-        }
+      variables: {},
+    };
 
-        axios.post("http://localhost:50308/graphql", body)
-            .then(res => {
-                console.log(res.data)
-                this.setState({ products: res.data.data.products, loading: false })
-            })
-    }
+    axios.post("http://localhost:50308/graphql", body).then((res) => {
+      console.log(res.data);
+      this.setState({ products: res.data.data.products, loading: false });
+    });
+  }
 
-    async deleteProduct(id) {
-        const body = {
-            query: `
+  async deleteProduct(id) {
+    const body = {
+      query: `
                     mutation {
                         status: deleteProduct(id: ${id})
                     }
                 `,
-            variables: {}
-        }
+      variables: {},
+    };
 
-        axios.post("http://localhost:50308/graphql", body)
-            .then(res => {
-                if (res.data.data.status) {
-                    this.getProducts();
-                }
-            })
-    }
+    axios.post("http://localhost:50308/graphql", body).then((res) => {
+      if (res.data.data.status) {
+        this.getProducts();
+      }
+    });
+  }
 }
--- HackDays.GraphQL/ClientApp/src/index.js
+++ HackDays.GraphQL/ClientApp/src/index.js
@@ -5,12 +5,52 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import { ApolloClient, InMemoryCache, split, HttpLink, ApolloProvider } from "@apollo/client";
+import { getMainDefinition } from "@apollo/client/utilities";
+import { WebSocketLink } from "@apollo/client/link/ws";
+
+const wsLink = new WebSocketLink({
+  uri: "ws://localhost:50308/graphql",
+  options: {
+    reconnect: true,
+  },
+});
+
+const httpLink = new HttpLink({
+  uri: "http://localhost:50308/graphql",
+});
+
+// The split function takes three parameters:
+//
+// * A function that's called for each operation to execute
+// * The Link to use for an operation if the function returns a "truthy" value
+// * The Link to use for an operation if the function returns a "falsy" value
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
+  },
+  wsLink,
+  httpLink
+);
+
+const client = new ApolloClient({
+  link: splitLink,
+  cache: new InMemoryCache(),
+});
+
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
 
+
 ReactDOM.render(
   <BrowserRouter basename={baseUrl}>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </BrowserRouter>,
   rootElement);
 
